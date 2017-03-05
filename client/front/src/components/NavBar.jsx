7ee88007075170a3bffb97074849d8e97c3d6308
An-l <an-l@qq.com>
2017-03-05 21:17:04 +0800

    修改前台样式

--- client/front/src/components/NavBar.jsx
+++ client/front/src/components/NavBar.jsx
@@ -11,28 +11,42 @@ class NavBar extends React.Component {
                     <li>
                         <Link to="/" title="首页">
                             <i className="iconfont icon-home"></i>
-                            <span>首页</span>
+                            <span className='text'>首页</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/archive" title="归档">
                             <i className="iconfont icon-archive"></i>
-                            <span>归档</span>
+                            <span className='text'>归档</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/tag" title="标签">
                             <i className="iconfont icon-tags"></i>
-                            <span>标签</span>
+                            <span className='text'>标签</span>
                         </Link>
                     </li>
                     <li>
                         <Link to="/about" title="关于">
                             <i className="iconfont icon-user"></i>
-                            <span>关于</span>
+                            <span className='text'>关于</span>
                         </Link>
                     </li>
                 </ul>
+                <div className='sider-nav'>
+                        <a rel="nofollow" target="_blank" href="https://github.com/An-l">
+                            <i title="GitHub" className="iconfont icon-github"></i>
+                        </a>
+                        <a rel="nofollow" target="_blank" href="http://weibo.com/annnl">
+                            <i title="Weibo" className="iconfont icon-zliconweibo01"></i>
+                        </a>
+                        <a href="/rss.xml">
+                            <i title="RSS" className="iconfont icon-rss"></i>
+                        </a>
+                        <a target="_blank" href="https://www.google.com/webhp#newwindow=1&amp;safe=strict&amp;q=site:http://an-l.cn">
+                            <i title="Search" className="iconfont icon-search"></i>
+                        </a>
+                </div>
             </nav>
         )
     }
